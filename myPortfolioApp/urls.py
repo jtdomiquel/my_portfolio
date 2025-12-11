18166f6664db372f28d69b37b33fd1c8319d6df2
Jomark Domiquel <jtdomiquel.aclcbutuan@gmail.com>
2025-12-11 15:10:04 +0800

    first commit

---
+++ myPortfolioApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [ 
+    path('home_page/', views.home_page, name='home_page'),
+    path('about_me/', views.about_me, name='about_me'),
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ myPortfolioApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def home_page(request):
+    return render(request, 'home_page.html')
+
+def about_me(request):
+    return render(request, 'about_me.html')
+    
